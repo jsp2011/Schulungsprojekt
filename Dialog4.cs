bfc4fb0f642d83bd95d96ef99c868729a14395c5
jsp2011 <juergen.spatz@humera.de>
2026-08-19 15:36:38 +0200

    Ändeurng Jürgen

--- Dialog4.cs
+++ Dialog4.cs
@@ -6,7 +6,7 @@ namespace SchulungsprojektGit
     {
         public Dialog4()
         {
-            Text = "Text für den vierter Dialog1";
+            Text = "Text für den vierter Dialog1 Jürgen";
 
             Label lblText = new Label
             {
